9d7f8bdabc5fc70f53feb89e7501d91a38a4506d
Karma Drukpa <karmadrukpa11@gmail.com>
2020-04-09 16:24:33 -0700

    Add chrome picker and buttons

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,37 +1,37 @@
 export default {
-    root: {
-        backgroundColor: "blue",
-        height: "100vh",
-        display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "center",
-    },
+	root: {
+		backgroundColor: 'blue',
+		height: '100vh',
+		display: 'flex',
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+	},
 
-    container: {
-        width: "50%",
-        display: "flex",
-        alignItems: "flex-start",
-        flexDirection: "column",
-        flexWrap: "wrap",
-    },
+	container: {
+		width: '50%',
+		display: 'flex',
+		alignItems: 'flex-start',
+		flexDirection: 'column',
+		flexWrap: 'wrap',
+	},
 
-    nav: {
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between",
-        color: "white",
-        alignItems: "center",
-        "& a": {
-            color: "white",
-            textDecoration: "none",
-        },
-    },
+	nav: {
+		display: 'flex',
+		width: '100%',
+		justifyContent: 'space-between',
+		color: 'white',
+		alignItems: 'center',
+		'& a': {
+			color: 'white',
+			textDecoration: 'none',
+		},
+	},
 
-    palettes: {
-        boxSizing: "border-box",
-        width: "100%",
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%",
-    },
+	palettes: {
+		boxSizing: 'border-box',
+		width: '100%',
+		display: 'grid',
+		gridTemplateColumns: 'repeat(3, 30%)',
+		gridGap: '5%',
+	},
 };
