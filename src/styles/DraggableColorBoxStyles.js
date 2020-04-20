12a502dad53610c2573c10d766a3ef87b00403bf
Karma Drukpa <karmadrukpa11@gmail.com>
2020-04-19 18:35:06 -0700

    moved styles file out and add constant file

---
+++ src/styles/DraggableColorBoxStyles.js
@@ -0,0 +1,35 @@
+const styles = {
+	root: {
+		width: '20%',
+		height: '25%',
+		margin: '0 auto',
+		display: 'inline-block',
+		position: 'relative',
+		cursor: 'pointer',
+		marginBottom: '-3.5px',
+	},
+
+	boxContent: {
+		position: 'absolute',
+		left: '0px',
+		width: '100%',
+		padding: '10px',
+		bottom: '0px',
+		letterSpacing: '1px',
+		textTransform: 'uppercase',
+		fontSize: '12px',
+		display: 'flex',
+		justifyContent: 'space-between',
+		color: 'rgba(0, 0, 0, 0.5)',
+		'&:hover svg': {
+			color: 'white',
+			transform: 'scale(1.5)',
+		},
+	},
+
+	deleteIcon: {
+		transition: 'all 0.3s ease-in-out',
+	},
+};
+
+export default styles;
