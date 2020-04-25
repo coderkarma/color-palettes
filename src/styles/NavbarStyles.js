7d798ab4021a1af771ad1a1e094dc6e7f8a0e4d0
Karma Drukpa <karmadrukpa11@gmail.com>
2020-04-25 01:09:20 -0700

    Make form and Navbar responsive

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
 	Navbar: {
 		display: 'flex',
@@ -18,12 +20,18 @@ export default {
 			textDecoration: 'none',
 			color: 'black',
 		},
+		[sizes.down('xs')]: {
+			display: 'none',
+		},
 	},
 
 	slider: {
 		width: '340px',
 		margin: '0 10px',
 		display: 'inline-block',
+		[sizes.down('md')]: {
+			width: '140px',
+		},
 	},
 
 	selectContainer: {
--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -1,4 +1,5 @@
 import { DRAWER_WIDTH } from '../../src/constants';
+import sizes from './sizes';
 
 const drawerWidth = DRAWER_WIDTH;
 
@@ -37,12 +38,19 @@ const styles = (theme) => ({
 		'& a ': {
 			textDecoration: 'none',
 		},
+		[sizes.down('xs')]: {
+			marginRight: '0.5rem',
+		},
 	},
 
 	button: {
-		margin: '0 0.5rems',
-		'& a': {
-			textDecoration: 'none',
+		margin: '0 0.5rem',
+		// '& a': {
+		// 	textDecoration: 'none',
+		// },
+		[sizes.down('xs')]: {
+			margin: '0 0.2rem',
+			padding: '0.3rem',
 		},
 	},
 });
